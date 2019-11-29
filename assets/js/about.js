f0e8e4872b060711772b2265b96b4807bae62391
rbiiiii <robinfoguenne@territoires-memoire.be>
2019-11-29 14:01:34 +0100

    add card anim + infos section

--- assets/js/about.js
+++ assets/js/about.js
@@ -1,13 +1,11 @@
-if(!!window.IntersectionObserver){
+if ('IntersectionObserver' in window) {
 	let observer = new IntersectionObserver((entries, observer) => { 
 		entries.forEach(entry => {
-			if(entry.intersectionRatio > 0.9) {
+			if(entry.intersectionRatio > 0.85) {
 				entry.target.classList.add('is-visible');
 				observer.unobserve(entry.target);
 			}
 		});
-	}, {threshold: 0.9});
+	}, {threshold: 0.85});
 	document.querySelectorAll('.observed').forEach(obj => { observer.observe(obj) });
-} else {
-	document.querySelectorAll('.observed').style.display = 'block';
 }
